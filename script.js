8df8d7918ca81e45550a1410726612ec389c1f53
sanz47 <syemaziz@iut-dhaka.edu>
2024-09-25 01:47:06 +0600

    About Me, Transition

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let letterIndex = 0;
 const typingSpeed = 100; // Speed of typing effect in milliseconds
 const pauseDuration = 1000; // Duration to pause between words in milliseconds
 const slideshowElement = document.getElementById("slideshow");
+    // Apply fade-in when the page is loaded
+    document.addEventListener("DOMContentLoaded", function() {
+        document.body.classList.add('fade-in');
+    });
+
+    // Add fade-out effect before leaving the page
+    window.addEventListener("beforeunload", function() {
+        document.body.style.opacity = 0;
+    });
+
+
 
 function typeWord() {
     const currentWord = words[wordIndex];
