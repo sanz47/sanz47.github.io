0224aeae0b0717a6aa91e1130953af5f0b7c73a8
sanz47 <syemaziz@iut-dhaka.edu>
2024-10-07 23:54:00 +0600

    changed the project to next.js

--- exp_script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const timelineItems = document.querySelectorAll('.timeline-item');
-
-    timelineItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('active');
-        });
-    });
-});
